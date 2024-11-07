1eb2035a1f75d0413c65bf7f7513c7dac8a52d16
CTunjano <caratungu@gmail.com>
2024-11-07 15:25:40 -0500

    ADD/ Comments

---
+++ Front/src/components/primary/comments/Comments.tsx
@@ -0,0 +1,12 @@
+import Post from '../../secondary/post/Post';
+import styles from './Comments.module.css';
+
+const Comments = () => {
+    return (
+        <div className={styles.container}>
+            <Post />
+        </div>
+    )
+}
+
+export default Comments;
